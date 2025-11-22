cae75a8e87c593f00a519f3bf5ad1694c4cfd2ca
ahosseini25 <ahosseini@cpp.edu>
2025-11-22 14:09:49 -0800

    fix sample_code.py

--- sample_code.py
+++ sample_code.py
@@ -1,10 +1,12 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
-# This is a test 
+
+# This is a test
 def multiply(a, b):
     return a * b
 
@@ -20,5 +22,3 @@ if __name__ == "__main__":
     print("Subtraction of 5 from 10 is:", subtract(10, 5))
     print("Multiplication of 4 and 5 is:", multiply(4, 5))
     print("Division of 10 by 2 is:", divide(10, 2))
-
-# test again test
